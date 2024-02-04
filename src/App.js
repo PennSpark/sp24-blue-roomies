fb49fcc94e4f23f514b9ba72f5bd738625cdc5e8
Gabriel Thiessen <gabrielthiessen@brb-07730.apn.wlan.private.upenn.edu>
2024-02-04 14:01:13 -0500

    Setting up home page, constants, assets, and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styles from './style'
 
-function App() {
+import { Billing, Business, Clients, Button, CardDeal, CTA, Feedback, Footer, GetStarted, Hero, NavBar, Stats, Testimonials} from './components'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/> 
+          <Billing />
+          <CardDeal/>
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer/>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
