a67afe0a8eee0f5cb8b12a858fad93239800111a
Gabriel Thiessen <gabrielthiessen@brb-11318.apn.wlan.private.upenn.edu>
2024-02-25 08:53:42 -0500

    Update on port connection

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -11,7 +11,7 @@ const LoginForm = () => {
 
   function handleSubmit(event) { 
     event.preventDefault()
-    axios.post('http://localhost:3000/login', {username, password})
+    axios.post('http://localhost:3036/login', {username, password})
     .then(res => {
       console.log(res)
       if (res.data) {
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     user: "sql5686573", 
     password: "dVx4szlrAB", 
     database: "sql5686573",
-    port: 3000
+    port: 3036
 })
 
 app.get('/', (req, res) => {
@@ -22,17 +22,15 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     const sql = "SELECT * FROM login WHERE username = ? AND password = ?"
     const values = [
-        req.body.email,
+        req.body.username,
         req.body.password
     ]
-    db.query(sql, [values], (err, data) => {
+    db.query(sql, values, (err, data) => {
         if (err) return res.json("Login failed");
         return res.json(data);
     })
 })
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(3036, () => {
     console.log("Listening...")
 })
