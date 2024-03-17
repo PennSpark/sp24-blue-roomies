2410db4d09dbae4e669b1b3118057c924217744c
tunamoon <tunamoon@sas.upenn.edu>
2024-03-17 16:13:33 -0400

    onclick

--- client/src/pages/LoginForm/LoginForm.js
+++ client/src/pages/LoginForm/LoginForm.js
@@ -47,7 +47,7 @@ const LoginForm = () => {
           <label><input type="checkbox" />Remember Me</label>
           <a href="#">Forgot Password</a>
         </div> */}
-        <button type="submit" className="btn" onClick1={handleClick}>
+        <button type="submit" className="btn" onClick={handleClick1}>
         Login</button>
         <div className="register-link">
           <p>Don't have an account? <a href="#"
